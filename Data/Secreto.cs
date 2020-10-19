5197efd5e80c1ec182fdcda273b202df28b0b2cf
RubenAlonzo <63566834+RubenAlonzo@users.noreply.github.com>
2020-10-19 17:25:02 -0400

    Configuracion inicial, tabla secretos, relacion, sqlite, etc...

---
+++ Data/Secreto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Session5.Data
+{
+    public class Secreto
+    {
+        public int SecretoId { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Lugar { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal ValorMonetario{ get; set; }
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
+        public int UsuarioId { get; set; }
+        public Usuario User { get; set; }
+    }
+}
---
+++ Data/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Session5.Data
+{
+    public class Usuario: IdentityUser
+    {
+        public List<Secreto> Secretos { get; set; }
+
+        
+    }
+}
